b7ac984f13e9fb2038310fbf144a94d9f1b48f17
David Yem <david13200arles@hotmail.fr>
2021-06-27 12:50:48 +0200

    mise en place des promises

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
+/*************************************************/
+/*Création de la fonction de connection*/
+function dbConnect(){
+    const connection = mysql.createConnection({
     host : 'localhost',
     user : 'groupomania',
     password : 'client',
     database : 'groupomania'
-})
+    })  
+    return connection
+}
 
-
+/*************************************************/
 /*Paramétrage du CORS*/
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','*')
@@ -17,21 +22,59 @@ app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH,OPTIONS')
     next()
 })
+
+
+/*************************************************/
 /*Paramétrage de l'analyseur de corps*/
 app.use(express.urlencoded({
     extended: true
 }))
 app.use(express.json())
 
+
+/*************************************************/
 /*Paramétrage des routes*/
 
+
+/*************************************************/
 /*POST*/
 app.post('/users',(req,res,next) =>{
     console.log(req.body)
     res.status(200).json({ message:'Compte utilisateur créer !'})
 })
 
-app.use('/chat',(req,res,next) => {
+app.get('/chat',(req,res,next) => {
+    const db = dbConnect()
+
+    db.promise().query('SELECT Users.nom AS nom,Users.prenom AS prenom,Chat.message AS message,DATE_FORMAT(Chat.date,GET_FORMAT(DATETIME,\'ISO\')) AS date FROM Users INNER JOIN Chat ON Users.userId = Chat.userId ORDER BY Users.id')
+
+    .then((responses,fields) => {
+        console.log(responses[0])
+        const resultats = responses[0]
+        res.status(200).json(resultats)
+    })
+    .catch((err) => {
+        res.status(500).json(err)
+    })
+})
+
+app.get('/publications',(req,res,next) =>{
+    const db = dbConnect()
+
+    db.promise().query('SELECT Users.nom AS nom,Users.prenom AS prenom,Publications.userId AS userId,Publications.message AS message,TIME(Publications.date) AS date FROM Users INNER JOIN publications ON Users.userId = Publications.userId ORDER BY Users.id')
+
+    .then((responses) => {
+        console.log(responses[0])
+        res.status(200).json(responses[0])
+    })
+    .catch((err) => {
+        res.status(500).json(err)
+    })
+})
+
+module.exports = app
+
+/*app.use('/chat',(req,res,next) => {
     connection.query(
         'SELECT Users.nom AS nom,Users.prenom AS prenom,Chat.message AS message,DATE_FORMAT(Chat.date,GET_FORMAT(DATETIME,\'ISO\')) AS date FROM Users INNER JOIN Chat ON Users.userId = Chat.userId ORDER BY Users.id',
         function(err,results,fields){
@@ -52,31 +95,4 @@ app.use('/chat',(req,res,next) => {
             res.status(200).json(data)
         }
     )
-})
-
-app.use('/publications',(req,res,next) => {
-    connection.query(
-        'SELECT Users.nom AS nom,Users.prenom AS prenom,Publications.userId AS userId,Publications.message AS message,TIME(Publications.date) AS date FROM Users INNER JOIN publications ON Users.userId = Publications.userId ORDER BY Users.id',
-        function(err,responses){
-            let data = []
-            if(err){
-                throw(err)
-            }
-            console.log(responses)
-            for(let response of responses){
-                let publication = {
-                    nom:response.nom,
-                    prenom:response.prenom,
-                    date:response.date,
-                    userId:response.userId,
-                    message:response.message
-                }
-                data.push(publication)
-            }
-            console.log(data)
-            res.status(200).json(data)
-        }
-    )
-})
-
-module.exports = app
+})*/
