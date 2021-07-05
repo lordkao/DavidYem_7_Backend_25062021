90d24ad91dde407fc591fbdb5053243d1341f679
David Yem <david13200arles@hotmail.fr>
2021-07-05 22:00:11 +0200

    Mise en place des commentaires  du code

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,9 +1,10 @@
 const database = require('../app.js')
+
+/*logique métier pour la gestion des messages*/
 exports.getAllMessagesChat = (req,res,next) => {
     const db = database.connect()
-
+    /*Obtention des messages du chat*/
     db.promise().query('SELECT Users.nom AS nom,Users.prenom AS prenom,Chat.id AS id,Chat.message AS message,DATE_FORMAT(Chat.date,GET_FORMAT(DATETIME,\'ISO\')) AS date,Chat.userId AS userId FROM Users INNER JOIN Chat ON Users.userId = Chat.userId ORDER BY date DESC LIMIT 10')
-
     .then((responses,fields) => {
         /*console.log(responses[0])*/
         const resultats = responses[0]
@@ -16,14 +17,17 @@ exports.getAllMessagesChat = (req,res,next) => {
 }
 exports.getMoreMessages = (req,res,next) => {
     const numberOfMessages = [req.params.numberOfMessages]
+    /*Obtention de plus de messages à afficher sur le front grâce à la variable numberOfMessages*/
     const string = `SELECT Users.nom AS nom,Users.prenom AS prenom,Chat.id AS id,Chat.userId AS userId,Chat.message AS message,DATE_FORMAT(Chat.date,GET_FORMAT(DATETIME,\'EUR\')) AS date FROM Users INNER JOIN Chat ON Users.userId = Chat.userId ORDER BY date DESC LIMIT 10 OFFSET ${numberOfMessages}`
     const db = database.connect()
     db.promise().query(string)
     .then((response) => {
+        /*Si plus de messages plusanciens à afficher*/
         if(response[0].length == 0){
             console.log(response[0].length)
             res.status(404).json({ message : 'Il n\' y a plus d\'anciens messages.'})
         }
+        /*Sinon on afficher les 10 prochains messages*/
         else{
             console.log(numberOfMessages)
             res.status(200).json(response[0])
@@ -39,6 +43,7 @@ exports.createMessageChat = (req,res,next) => {
     console.log(req.body)
     const db = database.connect()
     const infosMessage = [userId,message,date]
+    /*Création d'un message chat*/
     db.promise().query('INSERT INTO chat(userId,message,date) VALUES(?,?,?)',infosMessage)
     .then((response) => {
         console.log(response[0])
@@ -53,6 +58,7 @@ exports.deleteMessageChat = (req,res,next) => {
     console.log(req.params.id)
     const chat = [req.params.id]
     const db = database.connect()
+    /*Suppression de message*/
     db.promise().query('DELETE FROM Chat WHERE id=? ',chat)
     .then(() => { res.status(200).json({ message : 'Message supprimé avec succès !'})})
     .catch((err) => { res.status(500).json({ err })})
--- controllers/publications.js
+++ controllers/publications.js
@@ -4,9 +4,8 @@ const fs = require('fs')
 /*logique métier pour les publications*/
 exports.getAllPublications = (req,res,next) => {
     const db = database.connect()
-
+    /*Obtention des 10 dernières publications*/
     db.promise().query('SELECT Users.nom AS nom,Users.prenom AS prenom,Publications.id AS id,Publications.userId AS userId,Publications.message AS message,TIME(Publications.date) AS date,Publications.urlImage AS url FROM Users INNER JOIN publications ON Users.userId = Publications.userId ORDER BY Publications.date DESC LIMIT 10')
-
     .then((responses) => {
         return res.status(200).json(responses[0])
     })
@@ -21,6 +20,7 @@ exports.getMorePublications = (req,res,next) => {
     const requete = `SELECT Users.nom AS nom,Users.prenom AS prenom,Publications.message AS message,Publications.urlImage AS url,Publications.userId AS userId,DATE_FORMAT(Publications.date,GET_FORMAT(DATETIME,\'EUR\')) AS date,Publications.id AS id FROM Users INNER JOIN Publications ON Users.userId = Publications.userId ORDER BY Publications.date DESC LIMIT 10 OFFSET ${offset}`
     console.log(requete)
     const db = database.connect()
+    /*Obtention des 10 publications plus anciennes*/
     db.promise().query(requete)
     .then((response) => {
         if(response[0].length == 0){
@@ -33,8 +33,6 @@ exports.getMorePublications = (req,res,next) => {
     })
     .catch((err) => res.status(500).json(err))
     .then(() => db.end())
-    /*'SELECT Users.nom AS nom,Users.prenom AS prenom,Publications.message AS message,Publications.urlImage AS url,Publications.userId AS userId,DATE_FORMAT(Publications.date,GET_FORMAT(DATETIME,\'EUR\')) AS date,Publications.id AS id FROM Users INNER JOIN Publications ON Users.userId = Publications.userId ORDER BY Publications.date DESC LIMIT 10 OFFSET 10' */
-    /*res.status(200).json({ message:'Nous avons bien reçue votre demande ! Merci de patienter...'})*/
 }
 exports.createPublication = (req,res,next) => {
     console.log(req.body)
@@ -42,12 +40,7 @@ exports.createPublication = (req,res,next) => {
     const message = req.body.message
     const date = new Date()
     const publication = req.file ? [userId,message,date,`${req.protocol}://${req.get('host')}/images/${req.file.filename}`] : [userId,message,date]
-    /*const db = dbConnect()
-    db.promise().query('INSERT INTO publications(userId,message,date,urlImage) VALUES (?,?,?,?)',publication)
-    .then(() => { res.status(201).json({ message : 'Publication créée avec succès !'})})
-    .catch((err) => { res.status(500).json({ err })})
-    .then(() => db.end())*/
-    
+    /*Création d'une publication avec une condition s'il un fichier est joint*/ 
     if(req.file){
         const db = database.connect()
         db.promise().query('INSERT INTO publications(userId,message,date,urlImage) VALUES (?,?,?,?)',publication)
@@ -55,6 +48,7 @@ exports.createPublication = (req,res,next) => {
         .catch((err) => { res.status(500).json({ err })})
         .then(() => db.end())    
     }
+    /*Sinon on créer la publication sans fichier*/
     else{
         const db = database.connect()
         db.promise().query('INSERT INTO publications(userId,message,date) VALUES (?,?,?)',publication)
@@ -63,12 +57,14 @@ exports.createPublication = (req,res,next) => {
         .then(() => db.end())
     }
 }
-exports.updatePublication = (req,res,next) => {
+exports.updatePublication = (req,res,next) => {/*ce middleware a été créer si jamais le client souhaite implémenter la possibilité de modifier une publication mais qui pourrait fausser l'intégrité des données*/
     console.log(req.body)
     const userId = req.body.userId
     const message = req.body.message
     const date = new Date()
+    /*Condition pour déterminer la variable publication si un fichier est joint*/
     const publication = req.file ? [req.params.id,userId,message,date,`${req.protocol}://${req.get('host')}/images/${req.file.filename}`] : [req.params.id,userId,message,date]
+    /*Si un fichier est joint*/
     if(req.file){
         const db = database.connect()
         db.promise().query('UPDATE publications(userId,message,date,urlImage) WHERE id=? VALUES (?,?,?,?)',publication)
@@ -76,6 +72,7 @@ exports.updatePublication = (req,res,next) => {
         .catch((err) => { res.status(500).json({ err })})
         .then(() => db.end())    
     }
+    /*Sinon on met à jour la publication*/
     else{
         const db = database.connect()
         db.promise().query('UPDATE publications(userId,message,date) WHERE id=? VALUES (?,?,?)',publication)
@@ -83,11 +80,6 @@ exports.updatePublication = (req,res,next) => {
         .catch((err) => { res.status(500).json({ err })})
         .then(() => db.end())
     }
-    /*const db = dbConnect()
-    db.promise().query('UPDATE publications(userId,message,date,urlImage) WHERE id=? VALUES (?,?,?,?)',publication)
-    .then(() => { res.status(201).json({ message : 'Publication modifiée avec succès !'})})
-    .catch((err) => { res.status(500).json({ err })})
-    .then(() => db.end())*/
 }
 exports.deletePublication = (req,res,next) => {
     console.log(req.params.id)
@@ -212,6 +204,7 @@ exports.noteDislike = (req,res,next) => {
     const id = req.params.id
     const userId = req.params.userId
     const db = database.connect()
+    /*Vérification de la présence d'un userId dans la table Dislikes*/
     db.promise().query('SELECT * FROM Dislikes WHERE userId = ? AND publication = ?',[userId,id])
     .then((resultats) => {
         if(resultats[0].length == 0){
